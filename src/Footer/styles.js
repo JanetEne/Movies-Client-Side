23d78ce8160a9d02edee716a3b63c64adcac6cf4
JanetEne <janeteneogenyi@gmail.com>
2020-11-26 21:19:28 +0100

    footer design

---
+++ src/Footer/styles.js
@@ -0,0 +1,16 @@
+import styled from '@emotion/styled'
+
+export const Wrap = styled.div`
+  background-color: gainsboro;
+  height:63px;
+  border-top: 1px solid rgba(255, 255, 255, 0.15);
+  position: fixed;
+  width: 100%;
+  left: 0;
+  bottom: 0;
+`
+export const FooterText = styled.div`
+  color: Black;
+  text-align: center;
+  padding: 15px 0;
+`
