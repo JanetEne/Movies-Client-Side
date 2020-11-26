9c9e7f099cf333214a4a090bb7fee021c35ea674
JanetEne <janeteneogenyi@gmail.com>
2020-11-26 10:45:58 +0100

    delete jumbotron component

--- src/jumbotronComponent/style.js
+++
@@ -1,7 +0,0 @@
-/* eslint-disable import/prefer-default-export */
-import styled from '@emotion/styled';
-import { Jumbotron } from 'react-bootstrap';
-
-export const StyledJumbotron = styled(Jumbotron)`
-  background-color: blue;
-`;
